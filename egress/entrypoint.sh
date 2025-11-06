98cace170fe83a42fb09651372b08b159317496b
Navneet Karnani <navneet@mandraketech.in>
2025-11-06 23:10:02 +0530

    feat: Moved file concatenation from Dockerfile to entrypoint.sh for dynamic loading

--- egress/entrypoint.sh
+++ egress/entrypoint.sh
@@ -2,6 +2,26 @@
 echo "Clean up pid if it exists."
 rm -rf /var/run/squid.pid
 
+echo "Consolidating CRL regex lists..."
+CRL_REGEX_FILE="/etc/squid/crl-regex.txt"
+> "$CRL_REGEX_FILE" # Clear the file if it exists
+for file in /etc/squid/crl-lists.d/*.txt; do
+    if [ -f "$file" ]; then
+        cat "$file" >> "$CRL_REGEX_FILE"
+        echo "" >> "$CRL_REGEX_FILE" # Add a newline for separation
+    fi
+done
+
+echo "Consolidating domain lists..."
+CONSOLIDATED_DOMAINS_FILE="/etc/squid/all-allowed-domains.txt"
+> "$CONSOLIDATED_DOMAINS_FILE" # Clear the file if it exists
+for file in /etc/squid/domain-lists.d/*.txt; do
+    if [ -f "$file" ]; then
+        cat "$file" >> "$CONSOLIDATED_DOMAINS_FILE"
+        echo "" >> "$CONSOLIDATED_DOMAINS_FILE" # Add a newline for separation
+    fi
+done
+
 echo "Starting log watcher"
 tail -vn 0 -F /var/log/squid/access.log /var/log/squid/cache.log &
 
